6957e76002c45a9514c34c6787d83942b0b0f981
tungpt <thanhtung.uet@gmail.com>
2023-04-02 02:09:15 +0700

    config output

--- envrep/DEBIAN/preinst.sh
+++ envrep/DEBIAN/preinst.sh
@@ -6,15 +6,16 @@ if dpkg -s build-essential >/dev/null 2>&1; then
   echo "build-essential is already installed"
 else
   echo "build-essential is not installed. Installing it now..."
-  sudo apt-get update
-  sudo apt-get install -y build-essential
+  apt-get update
+  apt-get install -y build-essential
 fi
 
 # Biên dịch tệp nguồn C++
+echo "Compiling envrep.cpp -> /usr/local/bin/envrep"
 g++ -o /usr/local/bin/envrep /usr/local/src/envrep.cpp
-rm -rf /usr/local/src/envrep.cpp
 
 # Phân quyền cho tệp thực thi
+echo "Allow executing /usr/local/bin/envrep"
 chmod +x /usr/local/bin/envrep
 
 exit 0
